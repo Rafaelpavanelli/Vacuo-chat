d004b1a45bce1b5d3fcd32472071b15e56bd2649
Rafael <rafaelzica86@hotmail.com>
2022-07-29 15:24:17 -0300

    Criando o header da pagina home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,24 @@
-
-import {Link} from 'react-router-dom'
+import {} from './home.modules.css'
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
 function Home(){
   return(
+    <>
     <header>
       <span> <Link to="/">Vácuo</Link> </span>
       <ul>
-        <li>
-          <Link to='/Sobre'>Sobre</Link>
-        </li>
         <li>
           <Link to='/login'>Login</Link>
         </li>
       </ul>
     </header>
+    <body>
+      
+    </body>
+    <footer>
+      <span>Desenvolvido por Leonardo S. Oliveira e Rafael H. Pavanelli</span>
+    </footer>
+    </>
+
   )
 }
 export default Home;
